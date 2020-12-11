2b20abbbc5c768ff566b52a558b3ee37bbb03c60
alshafaeeahmed <alshafaeeahmed@gmail.com>
2020-12-11 15:09:33 -0800

    add about.html and index.html files

--- VolunteersForTheElderly/mysite/pages/views.py
+++ VolunteersForTheElderly/mysite/pages/views.py
@@ -5,8 +5,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1>Home Page</h2>')
+    return render(request, '/pages/index.html')
 
 
 def about(request):
-    return HttpResponse('<h1>About Page</h2>')
+    return render(request, '/pages/about.html')
